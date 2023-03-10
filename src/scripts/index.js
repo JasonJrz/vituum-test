050fa100d2ce2ad10a72577db5d5c73b42fd5208
JasonJrz <44707119+JasonJrz@users.noreply.github.com>
2023-03-09 22:25:21 -0500

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,55 +1,53 @@
-
-import fragmentShader from './shaders/fragment-shader.glsl'
-import vertexShader from './shaders/vertex-shader.glsl'
+import fragmentShader from './shaders/fragment-shader.glsl';
+import vertexShader from './shaders/vertex-shader.glsl';
 
 class App {
   constructor() {
+    console.log('Init');
 
-    console.log('Init')
-
-    this.addEventListeners()
-    this.addLinkListeners()
+    // this.addEventListeners()
+    // this.addLinkListeners()
   }
 
   onPopState() {
     this.onChange({
-      url: window.location.pathname
-    })
+      url: window.location.pathname,
+    });
   }
 
   async onChange({ url }) {
-    const request = await fetch(url)
-    
-    if(request.status === 200) {
-      const html = await request.text()
-      console.log(html)
+    const request = await fetch(url);
 
-      window.history.pushState({}, '', url)
+    if (request.status === 200) {
+      const html = await request.text();
+      console.log(html);
+
+      window.history.pushState({}, '', url);
     }
   }
 
   addEventListeners() {
-    console.log('Listeners Initiated')
-    window.addEventListener('popstate', () => this.onPopState())
+    console.log('Listeners Initiated');
+    window.addEventListener('popstate', () => this.onPopState());
   }
 
   addLinkListeners() {
-    const links = document.querySelectorAll('a')
-    
-    links.forEach(link => {
-      const isLocal = link.href.indexOf(window.location.origin) > -1
-    
-      if(isLocal) {
-        link.onclick = event => {
-          event.preventDefault()
+    const links = document.querySelectorAll('a');
+
+    links.forEach((link) => {
+      const isLocal = link.href.indexOf(window.location.origin) > -1;
+
+      if (isLocal) {
+        link.onclick = (event) => {
+          event.preventDefault();
 
           this.onChange({
-            url: link.href
-          })
-        }
+            url: link.href,
+          });
+        };
       }
-    })
+    });
   }
 }
 
-new App()
+new App();
